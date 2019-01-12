ebf7b0e864c2f2630b3ed135ae58c8348fc7d380
Vruizvil <vruiz@wittelecom.com>
2019-01-12 23:32:18 +0100

    v0.0.1

--- iota-transactions.js
+++ iota-transactions.js
@@ -6,44 +6,44 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node._sec = 2;
-	node._firstroot = '';
+	      node._firstroot = '';
 
-	console.log("Iota Api getTransactions: " + config.iotaNode);
+	      console.log("Iota Api getTransactions: " + config.iotaNode);
 
-	const iota = new IOTA({ provider: config.iotaNode })
+	      const iota = new IOTA({ provider: config.iotaNode });
         node.readyIota = true;
 
         node.on('input', function(msg) {
             if (this.readyIota) {
               let txt = JSON.stringify(msg.payload);
-	      let ascii = TRAN.transliterate(txt)
+	            let ascii = TRAN.transliterate(txt)
               let trytes = iota.utils.toTrytes(ascii)
 
               console.log("message payload: "+msg.payload)
-	      console.log("transliterated: "+ascii)
+	            console.log("transliterated: "+ascii)
               console.log("trytes: "+trytes)
 
               console.log("Uploading dataset via sendTransfer - please wait")
-	      const iota_addr = config.iotaAddr; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
-	      const iota_seed = config.iotaSeed; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
-	      const transfers = [
-		  {
-    			value: 0,
-    			address: iota_addr,
-    			message: trytes
-  		  }
-	      ]
+	            const iota_addr = config.iotaAddr; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
+	            const iota_seed = config.iotaSeed; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
+	            const transfers = [
+		              {
+    			             value: 0,
+    			             address: iota_addr,
+    			             message: trytes
+  		            }
+	            ]
               this.readyIota = false;
               var self = this;
               iota.api.sendTransfer(iota_seed, 14, 14, transfers, (error, success) => {
                 console.log("Report from iota node:")
-  		if (error) {
-    	 	  console.log(error);
-  		} else {
-    		  console.log(success);
-  		}
+  		            if (error) {
+    	 	             console.log(error);
+  		               } else {
+    		                 console.log(success);
+  		                   }
                 self.readyIota = true;
-	      });
+	             });
             }
         });
     }
