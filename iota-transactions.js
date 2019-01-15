18810cf9b2535aae104758aa5a4777aeff8e1d14
Vruizvil <vruiz@wittelecom.com>
2019-01-15 23:30:04 +0100

    V0.0.1

--- iota-transactions.js
+++ iota-transactions.js
@@ -17,27 +17,18 @@ module.exports = function(RED) {
             if (this.readyIota) {
               let txt = JSON.stringify(msg.payload);
 	            let ascii = TRAN.transliterate(txt)
-              let trytes = iota.utils.toTrytes(ascii)
 
               console.log("message payload: "+msg.payload)
 	            console.log("transliterated: "+ascii)
-              console.log("trytes: "+trytes)
 
-              console.log("Uploading dataset via sendTransfer - please wait")
+              console.log("Searching dataset via findTransactionObjects - please wait")
 	            const iota_addr = config.iotaAddr; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
-	            const iota_seed = config.iotaSeed; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
+	            const iota_hash = config.iotaHash; //'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
               const iota_tag = config.iotaTag; //Tag transaction
-              const transfers = [
-		              {
-    			             value: 0,
-    			             address: iota_addr,
-    			             message: trytes,
-                       tag: iota_tag
-  		            }
-	            ]
+
               this.readyIota = false;
               var self = this;
-              iota.api.sendTransfer(iota_seed, 14, 14, transfers, (error, success) => {
+              iota.api.findTransactionObjects({'addresses': [iota_addr], 'tags': [iota_tag], 'hashes' : [iota_hash]}, (error, success) => {
                 console.log("Report from iota node:")
   		            if (error) {
     	 	             console.log(error);
