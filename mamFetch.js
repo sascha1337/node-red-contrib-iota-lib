7994cec284a9cd24743efb4b9e505e19c41926ab
Vruizvil <vruiz@wittelecom.com>
2019-06-21 23:38:28 +0200

    v0.1.0
    mam include

---
+++ mamFetch.js
@@ -0,0 +1,37 @@
+const MAM = require('@iota/mam');
+const IOTA_CONVERTER = require('@iota/converter')
+
+module.exports = function(RED) {
+    function mamFetch(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.iotaNode = RED.nodes.getNode(config.iotaNode);
+        console.log("MAM Fetch INIT on iota node: " + node.iotaNode.host + ":" + node.iotaNode.port);
+        node.on('input', function(msg) {
+          config.root = msg.payload;
+
+          console.log("MAM fetch on iota node: " + node.iotaNode.host + ":" + node.iotaNode.port);
+          console.log("MAM root: " + config.root);
+          console.log("MAM mode: " + config.mode);
+          console.log("MAM secret: " + config.secret);
+          console.log("Fetching data ... ");
+
+          let mamState = MAM.init({ provider: node.iotaNode.host, 'port': node.iotaNode.port });
+          let root = config.root.slice(0,81);
+          if (config.mode == 'restricted' && config.secret.length == 0) {
+            console.log("Restricted mode: No MAM secret selected");
+          }
+          if (config.mode == 'public') {
+            config.secret = null;
+          }
+          let resp = MAM.fetch(root, config.mode, config.secret, (result) => {
+            let jsonArray = JSON.parse(IOTA_CONVERTER.trytesToAscii(result));
+            for (var i = 0; i < jsonArray.length; i++) {
+              node.send(jsonArray[i]);
+              // console.log(jsonArray[i])
+            }
+          }, config.limit);
+        });
+    }
+    RED.nodes.registerType("mamFetch",mamFetch);
+}
---
+++ mamPublish.js
@@ -0,0 +1,44 @@
+const MAM = require('@iota/mam');
+const IOTA_CONVERTER = require('@iota/converter');
+
+module.exports = function(RED) {
+    function mamPublish(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.iotaNode = RED.nodes.getNode(config.iotaNode);
+      	//const iota = new IOTA({'host': this.iotaNode.host, 'port': this.iotaNode.port});
+        console.log("MAM publish INIT on iota node: " + node.iotaNode.host + ":" + node.iotaNode.port);
+        node._state = MAM.init({ provider: node.iotaNode.host, 'port': node.iotaNode.port });
+        node._state = MAM.changeMode(node._state, config.mode, config.secretKey);
+        node.readyMAM = true;
+        node.arrayPackets = []
+
+        node.on('input', function(msg) {
+            // upload sensorTag's data packet: (msg.payload.json_data)
+            const packet = { time: Date.now(), data: msg.payload };
+            this.arrayPackets.push(packet);
+            console.log(this.arrayPackets.length);
+
+            if (this.readyMAM) {
+              let trytes = IOTA_CONVERTER.asciiToTrytes(JSON.stringify(this.arrayPackets));
+              let message = MAM.create(this._state, trytes);
+              // Update the mam state so we can keep adding messages.
+              this._state = message.state;
+
+              console.log("Uploading dataset via MAM - please wait");
+              console.log(message.address);
+              let resp = MAM.attach(message.payload, message.address);
+              this.readyMAM = false;
+              this.arrayPackets = [];
+              resp.then(function(result) {
+                 console.log(result) //will log results.
+                 node.readyMAM = true;
+                 node.send({payload: message.address});
+              });
+            } else {
+
+            }
+        });
+    }
+    RED.nodes.registerType("mamPublish",mamPublish);
+}
