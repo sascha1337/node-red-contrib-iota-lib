a95af999600669bb611c793ef91666e1cb1da6ae
Vruizvil <vruiz@wittelecom.com>
2019-06-24 16:46:01 +0200

    v0.1.3

--- iota-sendtransfer.js
+++ iota-sendtransfer.js
@@ -38,7 +38,7 @@ module.exports = function(RED) {
     			'value': iota_value,
     			'address': iota_addr,
     			'message': trytes,
-          'tag': asciitag
+          'tag': trytestag
   		  }]
               this.readyIota = false;
               this.status({fill:"red",shape:"ring",text:"connecting"});
