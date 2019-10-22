9542bcdfe46cf17a45ecc50837d366ad862388c8
Vruizvil <vruiz@wittelecom.com>
2019-10-22 18:30:07 +0200

    v0.2.4

--- mamPublish.js
+++ mamPublish.js
@@ -9,9 +9,12 @@ module.exports = function(RED) {
         node.iotaNode = RED.nodes.getNode(config.iotaNode);
       	//const iota = new IOTA({'host': this.iotaNode.host, 'port': this.iotaNode.port});
         //console.log("MAM publish INIT on iota node: " + node.iotaNode.host + ":" + node.iotaNode.port);
-        if (!isTrytes(config.channelseed,81) {
-          config.channelseed = Null;
-          console.log("Wrong user Channel Seed, generated random seed");
+        if (isTrytes(config.channelseed,81) {
+          console.log("Right User Channel Seed");
+          else {
+            config.channelseed = Null;
+            console.log("Wrong user Channel Seed, generated random seed");
+          }
         }
         node._state = MAM.init({ provider: node.iotaNode.host, 'port': node.iotaNode.port },config.channelseed,2);
         node._state = MAM.changeMode(node._state, config.mode, config.sidkey);
